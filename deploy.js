4db72b9e20d007ed80efd04aa2ee7c2f479657cf
ironhight <mrnguyenhoainam2468@gmail.com>
2022-11-27 22:31:51 +0700

    kickstart

---
+++ deploy.js
@@ -0,0 +1,18 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider('YOUR_MNEMONIC', 'YOUR_INFURA_URL');
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log('Attempting to deploy from account', accounts[0]);
+
+	const result = await new web3.eth.Contract(compiledFactory.abi).deploy({ data: '0x' + compiledFactory.evm.bytecode.object }).send({ from: accounts[0] });
+
+	console.log('Contract deployed to', result.options.address);
+	provider.engine.stop();
+};
+deploy();
